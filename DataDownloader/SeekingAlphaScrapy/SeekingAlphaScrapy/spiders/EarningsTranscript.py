73e6dd8bc1f4b4f10032cd9f20eb8f698f30b7c9
Faragó Balázs <minimalgeek@gmail.com>
2017-03-30 17:24:25 +0200

    Earnings transcript: only AAPL and autothrottle

--- DataDownloader/SeekingAlphaScrapy/SeekingAlphaScrapy/spiders/EarningsTranscript.py
+++ DataDownloader/SeekingAlphaScrapy/SeekingAlphaScrapy/spiders/EarningsTranscript.py
@@ -1,20 +1,23 @@
 import json
 import scrapy
 
+
 class EarningstranscriptSpider(scrapy.Spider):
     name = "EarningsTranscript"
     allowed_domains = ["seekingalpha.com"]
     article_url_base = 'http://seekingalpha.com'
     custom_settings = {
-        'ITEM_PIPELINES' : {
+        'ITEM_PIPELINES': {
             'SeekingAlphaScrapy.pipelines.MongoPipeline': 100,
         },
-        'MONGO_COLLECTION' : 'earnings_transcript',
-        'DOWNLOAD_DELAY' : 5
+        'MONGO_COLLECTION': 'earnings_transcript',
+        'DOWNLOAD_DELAY': 20,
+        'CONCURRENT_REQUESTS': 1,
     }
 
     def start_requests(self):
-        tickers = json.loads(open('tickers.json', encoding='utf-8').read())
+        #tickers = json.loads(open('tickers.json', encoding='utf-8').read())
+        tickers = [{'Symbol': 'AAPL'}]
         for ticker in tickers:
             urlroot = 'http://seekingalpha.com/symbol/' + \
                 ticker['Symbol'] + '/earnings/more_transcripts?page='
@@ -31,12 +34,12 @@ class EarningstranscriptSpider(scrapy.Spider):
 
         for resp in response.xpath("//a[@sasource]/@href").extract():
             yield scrapy.Request(self.article_url_base + resp[2:-2], self.parse_article,
-                                 meta={'ticker': response.meta['ticker']})
+                                 meta=response.meta)
 
     def parse_article(self, response):
         yield {'url': response.url,
                'tradingSymbol': response.meta['ticker'],
-               'publishDate':response.xpath('//time[@content]/@content').extract_first(),
+               'publishDate': response.xpath('//time[@content]/@content').extract_first(),
                'rawText': ' '.join(map(str, response.css('div.sa-art p *::text').extract())),
                'qAndAText': ' '.join(map(str, response.css('div.sa-art #question-answer-session~ p *::text').extract()))
-              }
+               }
