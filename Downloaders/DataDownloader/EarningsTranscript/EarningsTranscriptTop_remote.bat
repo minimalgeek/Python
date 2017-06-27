ff8e6bec1917d02c6ff2fa6ebff3568a882a1edc
octogon21 <czachesz@otpalap.hu>
2017-06-27 14:57:48 +0200

    minor changes

--- Downloaders/DataDownloader/EarningsTranscript/EarningsTranscriptTop_remote.bat
+++ Downloaders/DataDownloader/EarningsTranscript/EarningsTranscriptTop_remote.bat
@@ -1,1 +1,1 @@
-scrapy crawl EarningsTranscriptTop -s MONGO_URI=mongodb://192.168.137.62:27017 -s MONGO_DATABASE=insider -s MONGO_COLLECTION=earnings_call_Nas100_Broad_NEW -s MODE=SINGLE -s TICKER=ADI
+scrapy crawl EarningsTranscriptTop -s MONGO_URI=mongodb://192.168.137.62:27017 -s MONGO_DATABASE=insider -s MONGO_COLLECTION=earnings_call_Nas100_Broad_Manual_Update -s MODE=SINGLE -s TICKER=PDCO
