a9a3b2486a05543fcfaf7a20f10a1bdf3da9cb2d
octogon21 <czachesz@otpalap.hu>
2017-07-17 10:33:02 +0200

    EA days filtering

--- Mongo scripts/Zacks_Dates_Grouping_from_Today_filtered.js
+++ Mongo scripts/Zacks_Dates_Grouping_from_Today_filtered.js
@@ -13,20 +13,39 @@ db.zacks_earnings_call_dates.aggregate(
 
 		// Stage 2
 		{
+			$lookup: {
+			    from: "tickers",
+			    localField: "ticker",
+			    foreignField: "ticker",
+			    as: "matching_tickers"
+			}
+		},
+
+		// Stage 3
+		{
+			$match: { 
+			     matching_tickers: { 
+			           $ne: [] 
+			     } 
+			}
+		},
+
+		// Stage 4
+		{
 			$group: {
 			    _id: {day: "$nextReportDate"},
 			    numb: {$sum: 1}
 			}
 		},
 
-		// Stage 3
+		// Stage 5
 		{
 			$sort: {
 			_id:1
 			}
 		},
 
-		// Stage 4
+		// Stage 6
 		{
 			$project: {
 			      _id:0,
