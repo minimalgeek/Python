b730f505d25b2a3cdafacefa15b7c6b36506870c
Faragó Balázs <minimalgeek@gmail.com>
2017-08-07 14:25:34 +0200

    Extra options parametrized in URLs
    Only for Earnings Transcript downloader

--- Downloaders/DataDownloader/EarningsTranscript/index.py
+++ Downloaders/DataDownloader/EarningsTranscript/index.py
@@ -7,6 +7,11 @@ from scrapy.utils.project import get_project_settings
 app = Flask(__name__)
 
 
+@app.route('/')
+def hello():
+    return 'Hello'
+
+
 @app.route('/zacks')
 def zacks():
     process = CrawlerProcess(get_project_settings())
@@ -21,6 +26,20 @@ def zacks():
 def earnings():
     process = CrawlerProcess(get_project_settings())
     process.settings.set('MONGO_URI', 'mongodb://db:27017', 1000)  # Docker
+    process.crawl('EarningsTranscriptTop')
+    process.start()
+    return 'success'
+
+
+@app.route('/earnings/<database>/<collection>/<mode>/', defaults={'ticker': None})
+@app.route('/earnings/<database>/<collection>/<mode>/<ticker>')
+def earnings_parametrized(database, collection, mode, ticker):
+    process = CrawlerProcess(get_project_settings())
+    process.settings.set('MONGO_URI', 'mongodb://db:27017', 1000)  # Docker
+    process.settings.set('MONGO_DATABASE', database, 1000)
+    process.settings.set('MONGO_COLLECTION', collection, 1000)
+    process.settings.set('MODE', mode, 1000)
+    process.settings.set('TICKER', ticker, 1000)
     process.crawl('EarningsTranscriptTop')
     process.start()
     return 'success'
